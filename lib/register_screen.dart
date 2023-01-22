6b686a5680dfdf3ad4cea10a714c46a8aa462af6
imaldi <aldiirsanmajid@gmail.com>
2023-01-22 15:19:37 +0700

    done refactoring register form

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -20,76 +20,115 @@ class _RegisterScreenState extends State<RegisterScreen> {
   var password = "";
   var address = "";
   var phoneNumber = "";
+  var formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Register Screen"),),
-      body: Center(child: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Text("Silahkan Registrasi Dahulu"),
-              TextFormField(
-                controller: nameController,
-                // decoration: InputDecoration(hintText: "Nama Lengkap"),
-                decoration: InputDecoration(label: Text("Nama Lengkap")),
+      appBar: AppBar(
+        title: Text("Register Screen"),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text("Silahkan Registrasi Dahulu"),
+                  TextFormField(
+                    controller: nameController,
+                    validator: (value) {
+                      if ((value?.length ?? 0) > 20) {
+                        return "Nama Tidak Boleh Lebih Dari 5 Karakter";
+                      }
+                      return null;
+                    },
+                    onSaved: (val) {
+                      nama = nameController.text;
+                      nameController.text = "";
+                    },
+                    // decoration: InputDecoration(hintText: "Nama Lengkap"),
+                    decoration: InputDecoration(label: Text("Nama Lengkap")),
+                  ),
+                  TextFormField(
+                    controller: usernameController,
+                    validator: (val) {
+                      if ((val?.isEmpty ?? false)) {
+                        return "Field ini tidak boleh kosong";
+                      }
+                      return null;
+                    },
+                    onSaved: (val) {
+                      username = usernameController.text;
+                      usernameController.text = "";
+                    },
+                    decoration: InputDecoration(label: Text("Username")),
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onSaved: (val) {
+                      email = emailController.text;
+                      emailController.text = "";
+                    },
+                    decoration: InputDecoration(label: Text("Email")),
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    onSaved: (val) {
+                      password = passwordController.text;
+                      passwordController.text = "";
+                    },
+                    decoration: InputDecoration(label: Text("Password")),
+                  ),
+                  TextFormField(
+                    controller: addressController,
+                    decoration: InputDecoration(label: Text("Address")),
+                    onSaved: (val) {
+                      address = addressController.text;
+                      addressController.text = "";
+                    },
+                  ),
+                  TextFormField(
+                    controller: phoneNumberController,
+                    keyboardType: TextInputType.phone,
+                    onSaved: (val) {
+                      phoneNumber = phoneNumberController.text;
+                      phoneNumberController.text = "";
+                    },
+                    decoration: InputDecoration(label: Text("Phone Number")),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if ((formKey.currentState?.validate() ?? false)) {
+                          setState(() {
+                            formKey.currentState?.save();
+                          });
+                        }
+                      },
+                      child: Text("Register")),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Text("Hasil Submit Form:"),
+                  Text("Name: ${nama}"),
+                  Text("UserName: ${username}"),
+                  Text("Email: ${email}"),
+                  Text("Password: ${password}"),
+                  Text("Address: ${address}"),
+                  Text("Phone Number: ${phoneNumber}"),
+                ],
               ),
-              TextFormField(
-                controller: usernameController,
-                decoration: InputDecoration(label: Text("Username")),
-              ),
-              TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(label: Text("Email")),
-              ),
-              TextFormField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(label: Text("Password")),
-              ),
-              TextFormField(
-                controller: addressController,
-                decoration: InputDecoration(label: Text("Address")),
-              ),
-              TextFormField(
-                controller: phoneNumberController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                    label: Text("Phone Number")),
-              ),
-              SizedBox(height: 32,),
-              ElevatedButton(onPressed: (){
-                setState(() {
-                  nama = nameController.text;
-                  username = usernameController.text;
-                  email = emailController.text;
-                  password = passwordController.text;
-                  address = addressController.text;
-                  phoneNumber = phoneNumberController.text;
-                  nameController.text = "";
-                  usernameController.text = "";
-                  emailController.text = "";
-                  passwordController.text = "";
-                  addressController.text = "";
-                  phoneNumberController.text = "";
-                });
-              }, child: Text("Register")),
-              SizedBox(height: 32,),
-              Text("Hasil Submit Form:"),
-              Text("Name: ${nama}"),
-              Text("UserName: ${username}"),
-              Text("Email: ${email}"),
-              Text("Password: ${password}"),
-              Text("Address: ${address}"),
-              Text("Phone Number: ${phoneNumber}"),
-            ],
+            ),
           ),
         ),
-      ),),
+      ),
     );
   }
 }
-
